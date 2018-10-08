140359a2144d6d7662b1c4cdbb05208cf7e7ffbe
Pavel Korolev <dev@borodust.org>
2018-10-08 09:21:51 +0300

    Let claw define implicit package

--- claw.lisp
+++ claw.lisp
@@ -1,9 +1,5 @@
-(cl:defpackage :%mecab
-  (:use))
-
 (claw:c-include "bodge_mecab.h" bodge-mecab
   :in-package :%mecab
-  :local-only t
   :sysincludes (:mecab-includes)
   :include-definitions ("(mecab|MECAB)_\\w*")
   :rename-symbols (claw:by-removing-prefixes "mecab_" "MECAB_"))
